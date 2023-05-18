29f3bb6b5809c872c7100c061d83716b4b58c5fc
yqlbu <yqlbu@bu.edu>
2023-05-19 00:27:43 +0800

    add template code for concurrency

---
+++ concurrency/template/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "sync"
+
+func main() {
+	// business logics
+	rawInputs, _, err := //some api data (as a List)
+
+	wg := sync.WaitGroup{}
+	itemList := []*model.Item{}
+
+	wg.Add(N) // initiate N number of goroutines
+	chanOut := make(chan *Item, len(rawInputs))
+
+	for index, item := range rawInputs {
+		go func(index int, item any) {
+			newItem := // ...
+			// business logics
+			chanOut <- newItem // assign value to channel
+			wg.Done() // remove the goroutine(thead) from the waitGroup
+		}(index, item)
+	}
+	wg.Wait() // wait for all thead to finish
+	close(chanOut) // close channel
+
+
+	for {
+		item, open := <- chanOut // return channel output and assign it to a variable
+		if !open { break }
+		itemsList = append(tagsList, item)
+	}
+
+	fmt.Println(itemsList)
+}
