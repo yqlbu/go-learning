66bcec8f9da649b38020d97e657daffb0afcb75c
yqlbu <yqlbu@bu.edu>
2023-05-23 20:17:35 +0800

    use pointer receiver for the interface

--- factory-interface-pattern/main.go
+++ factory-interface-pattern/main.go
@@ -11,13 +11,13 @@ type person struct {
 	age  int
 }
 
-func (p person) Greet() {
+func (p *person) Greet() {
 	fmt.Printf("Hi! My name is %s", p.name)
 }
 
 // Here, NewPerson returns an interface, and not the person struct itself
 func NewPerson(name string, age int) Person {
-	return person{
+	return &person{
 		name: name,
 		age:  age,
 	}
