66758afc72ad5bbd5dc44d1010d5b1479a5b49aa
kev <yqlbu@bu.edu>
2023-08-23 10:11:46 +0800

    feat(ants): use zerolog

--- concurrency/ants/go.mod
+++ concurrency/ants/go.mod
@@ -2,4 +2,13 @@ module ants
 
 go 1.21.0
 
-require github.com/panjf2000/ants/v2 v2.8.1
+require (
+	github.com/panjf2000/ants/v2 v2.8.1
+	github.com/rs/zerolog v1.30.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+)
--- concurrency/ants/main.go
+++ concurrency/ants/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 	"sync/atomic"
 	"time"
 
 	"github.com/panjf2000/ants/v2"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 var sum int32
@@ -14,19 +15,20 @@ var sum int32
 func myFunc(i interface{}) {
 	n := i.(int32)
 	atomic.AddInt32(&sum, n)
-	fmt.Printf("run with %d\n", n)
+	log.Printf("run with %d\n", n)
 }
 
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	defer ants.Release()
 
 	runTimes := 1000
+	var wg sync.WaitGroup
 
 	// Use the common pool.
-	var wg sync.WaitGroup
 	syncCalculateSum := func() {
 		time.Sleep(10 * time.Millisecond)
-		fmt.Println("Hello World!")
+		log.Info().Msg("Hello World!")
 		wg.Done()
 	}
 	for i := 0; i < runTimes; i++ {
@@ -34,15 +36,18 @@ func main() {
 		_ = ants.Submit(syncCalculateSum)
 	}
 	wg.Wait()
-	fmt.Printf("running goroutines: %d\n", ants.Running())
-	fmt.Printf("finish all tasks.\n")
+	log.Printf("running goroutines: %d\n", ants.Running())
+	log.Printf("finish all tasks.\n")
 
 	// Use the pool with a function,
 	// set 10 to the capacity of goroutine pool and 1 second for expired duration.
 	p, _ := ants.NewPoolWithFunc(10, func(i interface{}) {
 		myFunc(i)
+		// , ants.WithLogger(&log.Logger)
 		wg.Done()
-	})
+	},
+		ants.WithLogger(&log.Logger),
+	)
 	defer p.Release()
 	// Submit tasks one by one.
 	for i := 0; i < runTimes; i++ {
@@ -50,6 +55,6 @@ func main() {
 		_ = p.Invoke(int32(i))
 	}
 	wg.Wait()
-	fmt.Printf("running goroutines: %d\n", p.Running())
-	fmt.Printf("finish all tasks, result is %d\n", sum)
+	log.Printf("running goroutines: %d\n", p.Running())
+	log.Printf("finish all tasks, result is %d\n", sum)
 }
